cc71e5b65019326ad68cd6c9ec6eb3e4238911ee
denis.chainikov <denis.chaynikov@phystech.edu>
2019-11-02 14:37:18 +0100

    first commit

---
+++ ICP.cpp
@@ -0,0 +1,12 @@
+#include "ICP.h"
+
+void nearest_neighbour(const MatrixXd &V1, const MatrixXd &V2, MatrixXd &nn_V2){
+  // return the nearest neighbour to V1 in V2 as nn_V2
+  // Complete here
+}
+
+
+void transform(MatrixXd &V1,const MatrixXd &V2){
+  //align V1 to V2 when V1 and V2 points are in correspondance
+  // complete here
+}
