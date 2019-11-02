cad755b2c88caae45fec3a7432b25b2fca6f57ce
denis.chainikov <denis.chaynikov@phystech.edu>
2019-11-02 17:06:26 +0100

    Implemented transform function which find rotation and translation and apply it for correspondant sets of points

--- ICP.cpp
+++ ICP.cpp
@@ -1,12 +1,32 @@
 #include "ICP.h"
 
 void nearest_neighbour(const MatrixXd &V1, const MatrixXd &V2, MatrixXd &nn_V2){
-  // return the nearest neighbour to V1 in V2 as nn_V2
-  // Complete here
+
 }
 
 
 void transform(MatrixXd &V1,const MatrixXd &V2){
-  //align V1 to V2 when V1 and V2 points are in correspondance
-  // complete here
+  // find a mean value for two sets of points
+  VectorXd d = V2.colwise().mean().transpose();
+  VectorXd m = V1.colwise().mean().transpose();
+
+  // calculation deltas
+  MatrixXd delta_d = V2;
+  MatrixXd delta_m = V1;
+
+  delta_d.rowwise() -= d.transpose();
+  delta_m.rowwise() -= m.transpose();
+
+  // getting a correlation matrix
+  MatrixXd corr = delta_m.transpose() * delta_d;
+
+  // SVD and rotation
+  JacobiSVD<MatrixXd> svd(corr, ComputeThinU | ComputeThinV);
+  MatrixXd rot = svd.matrixV() * svd.matrixU().transpose();
+
+  // translation
+  VectorXd transl = d - rot * m;
+
+  // change V1
+  V1 = ((rot * V2.transpose()).colwise() + transl).transpose();
 }
