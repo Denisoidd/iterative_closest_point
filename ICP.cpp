2778bacf5071ea226c8b35f4f4e1c4ff038db3f0
denis.chainikov <denis.chaynikov@phystech.edu>
2019-11-03 15:26:44 +0100

    Changed transform function. Implementer k-nn

--- ICP.cpp
+++ ICP.cpp
@@ -1,7 +1,19 @@
 #include "ICP.h"
+#include <iostream>
+#include <ostream>
 
 void nearest_neighbour(const MatrixXd &V1, const MatrixXd &V2, MatrixXd &nn_V2){
+  std::vector<std::vector<int > > O_PI;
+  Eigen::MatrixXi O_CH;
+  Eigen::MatrixXd O_CN;
+  Eigen::VectorXd O_W;
+  igl::octree(V2,O_PI,O_CH,O_CN,O_W);
+  Eigen::MatrixXi I;
 
+  igl::knn(V1, 2, O_PI, O_CH,O_CN, O_W, I);
+  for (int i = 0; i < V1.rows(); i++){
+      nn_V2.row(i) = V2.row(I(i, 1));
+    }
 }
 
 
@@ -23,10 +35,17 @@ void transform(MatrixXd &V1,const MatrixXd &V2){
   // SVD and rotation
   JacobiSVD<MatrixXd> svd(corr, ComputeThinU | ComputeThinV);
   MatrixXd rot = svd.matrixV() * svd.matrixU().transpose();
-
+  std::cout << "SVD V" << '\n';
+  std::cout << svd.matrixV() << '\n';
+  std::cout << "SVD U" << '\n';
+  std::cout << svd.matrixU() << '\n';
+  std::cout << "Rotation" << '\n';
+  std::cout << rot << '\n';
   // translation
   VectorXd transl = d - rot * m;
+  std::cout << "Translation" << '\n';
+  std::cout << transl << '\n';
 
   // change V1
-  V1 = ((rot * V2.transpose()).colwise() + transl).transpose();
+  V1 = ((rot * V1.transpose()).colwise() + transl).transpose();
 }
